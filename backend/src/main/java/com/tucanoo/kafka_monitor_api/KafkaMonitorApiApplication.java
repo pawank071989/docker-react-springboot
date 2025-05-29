a3c77f70b63120b22faf28c0ce7aa8099c8f405a
pawank071989 <pawank071989@gmail.com>
2025-05-29 13:02:40 +0530

    Update KafkaMonitorApiApplication.java
    Added URL

--- backend/src/main/java/com/tucanoo/kafka_monitor_api/KafkaMonitorApiApplication.java
+++ backend/src/main/java/com/tucanoo/kafka_monitor_api/KafkaMonitorApiApplication.java
@@ -7,7 +7,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @EnableScheduling
 public class KafkaMonitorApiApplication {
-
+     //https://medium.com/@swaroop1232010/building-a-real-time-delivery-tracking-system-using-kafka-with-spring-boot-and-react-js-6aa2779081f6
     public static void main(String[] args) {
         SpringApplication.run(KafkaMonitorApiApplication.class, args);
     }
